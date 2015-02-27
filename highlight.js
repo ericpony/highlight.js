8404ecea642f7e53c1a8ca6d18514358ed526801
ericpony <ericpony@csie.org>
2015-02-28 01:28:15 +0800

    refactor and improve performance

--- highlight.js
+++ highlight.js
@@ -109,174 +109,136 @@
     }
   }
 
-  function createHighlightedCode(lang, element, options)
-  {
-    function gen_id() { return Math.random().toString().substr(2,5) }
-    function gen_name(s,t) { return 'r' + s.id + '-' + t }
-    function unescape(text) {
-      return text
-      .replace(/&amp;/g, '&')
-      .replace(/&lt;/g, '<')
-      .replace(/&gt;/g, '>')
-      .replace(/&quot;/g, '"')
-      .replace(/&nbsp;/g, ' ');
+  function createSyntaxRules(lang) {
+    var syntax = LANG[lang];
+    if(!syntax) return null;
+    if(syntax.processed) return syntax;
+    function regex(str) {
+      if(!str) return '';
+      return '\\b(?:' + str.replace(/ /g, '|') + ')\\b';
     }
-    function escape(text) {
-      return text
-      .replace(/&/g, '&amp;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;')
-      .replace(/ /g, '&nbsp;');
+    function regexp(pattern) {
+      if(typeof pattern != 'string') return pattern;
+      return new RegExp(regex(pattern), 'g');
     }
-    function tab2spaces(text) {
-      var spaces = '    ';
-      var stext = '';
-      var lines = unescape(text).replace(/\n$/g, '').split('\n');
-      for(var i = 0; i < lines.length; i++) {
-        for(var p = 0; (p = lines[i].indexOf('\t')) != -1;)
-          lines[i] = lines[i].replace(/\t/, spaces.slice(0, 4-p%4));
-        stext += lines[i] + '\n';
-      }
-      return stext;
-    }
-    var scopes = (function(){
-      var stack = [];
-      return {
-        id:      gen_id(),
-        init:    function() { this.create(); return this },
-        destroy: function() { stack.pop(); this.update_regex(); },
-        current: function() { return stack[stack.length-1] },
-        create:  function(is_block) { 
-          if(is_block && !this.current().is_block)
-            this.current().is_block = true;
-          else
-            stack.push({ id:this.id+'-'+gen_id(), nominals:{}, nominal_regex:'', is_block:!stack.length }); 
-        },
-        lookup:  function(name) {
-          for(var i=stack.length-1; i>=0; i--)
-            if(stack[i].nominals[name]) return stack[i];
-        },
-        add_nominal: function(name) {
-          var scope = this.current();
-          if(scope.nominals[name]) return;
-          scope.nominal_regex = !scope.nominal_regex ? ('\\b' + name + '\\b') : (scope.nominal_regex + '|\\b' + name + '\\b');
-          scope.nominals[name] = true;
-          this.update_regex();
-        },
-        update_regex: function() {
-          var syntax  = LANG[lang];
-          var regexes = [];
-          for(var i=0; i<stack.length; i++) 
-            if(stack[i].nominal_regex)
-              regexes.push(stack[i].nominal_regex);
-          if(!regexes.length) return;
-          var r = new RegExp(regexes.join('|'), 'g');
-          r.index     = syntax.nominal.r.index;
-          r.lastIndex = syntax.nominal.r.lastIndex;
-          syntax.nominal.r = r;
-        }
-      };
-    })();
-    options = options || {};
-    cache = {};
-    cache.scopes = scopes.init();
-    var text = tab2spaces(element.innerHTML);
-    var last_index = 0, nominal_index, attr;
-    var token1, token2, token3;
-    var regexps = (function (SYNTAX) {
-      var ret = [];
-      for(var rule in COMMON) ret.push(COMMON[rule]);
-      for(var rule in SYNTAX) SYNTAX[rule] && ret.push(SYNTAX[rule]) && rule=='nominal' && (nominal_index=ret.length-1);
-      cache.m = ret.map(function(){ return 0 });
-      return ret;
-    })((function () {
-      var syntax = LANG[lang];
-      if(!syntax) return null;
-      if(syntax.processed) return syntax;
-      function regex(str) {
-        if(!str) return '';
-        return '\\b(?:' + str.replace(/ /g, '|') + ')\\b';
-      }
-      function regexp(pattern) {
-        if(typeof pattern != 'string') return pattern;
-        return new RegExp(regex(pattern), 'g');
-      }
-      function create_nominal(nominal, parameters, index) {
-        if(nominal) {
-          var scope = cache.scopes.current();
-          var name = 'r' + scope.id + '-' + nominal;
-          var attr = {
-            onmouseover:  function(e) { change_style(name, true) },
-            onmouseleave: function(e) { change_style(name, false) },
-          };
-          attr.className = STYLE.type + ' ' + name;
-          attr.name      = name;
-          colorize(nominal, undefined, attr);
-          cache.scopes.add_nominal(nominal);
-          cache.m[index] = 0; // reset the last position of searched text for nominal rule
-        }
-        if(parameters !== undefined) { // output '()' when parameters is ''
-          colorize('(');
-          cache.scopes.create();
-          if(parameters) {
-            var rr, r = new RegExp(/([$\w]+)([^,]*,?\W*)/g);
-            var names = [];
-            while ((rr = r.exec(parameters)) !== null) {
-              create_nominal(rr[1], undefined, index);
-              names.push(rr[1]);
-              colorize(rr[2]);
-            }
-          }
-          colorize(')');
-        }
-      }
-      function create_ref(nominal, parameters, index) {
-        var scope = cache.scopes.lookup(nominal);
-        if(!scope) return create_nominal(nominal, parameters, index);
-        var name = gen_name(scope, nominal);
-        attr = {
-          className:    STYLE.nominal + ' ' + name,
-          onclick:      function(e) { location.href = '#' + name },
+    function create_nominal(nominal, parameters, index) {
+      if(nominal) {
+        var scope = cache.scopes.current();
+        var name = 'r' + scope.id + '-' + nominal;
+        var attr = {
           onmouseover:  function(e) { change_style(name, true) },
           onmouseleave: function(e) { change_style(name, false) },
         };
+        attr.className = STYLE.type + ' ' + name;
+        attr.name      = name;
         colorize(nominal, undefined, attr);
+        cache.scopes.add_nominal(nominal);
       }
-
-      syntax.processed = true;
-      syntax.keyword   = syntax.keyword   ? { r: regexp(syntax.keyword), css: STYLE.keyword } : {};
-      syntax.type      = syntax.type      ? { r: regexp(syntax.type),    css: STYLE.type } : {};
-      syntax.built_in  = syntax.built_in  ? { r: regexp(syntax.built_in),css: STYLE.built_in } : {};
-      syntax.type_ctor = syntax.type_ctor ? { r: syntax.type_ctor, update: create_nominal } : {};
-      syntax.ref_ctor  = syntax.ref_ctor  ? { r: syntax.ref_ctor,  update: create_nominal } : {};
-      syntax.nominal   = { r: '', css: STYLE.nominal, update: create_ref };
-      return syntax;
-    })());
-
-    cache.codeArea = options['lineno'] ? document.createElement('OL') : document.createElement('UL');
-
-    while(1) {
+      if(parameters !== undefined) { // output '()' when parameters is ''
+        colorize('(');
+        cache.scopes.create();
+        if(parameters) {
+          var rr, r = new RegExp(/([$\w]+)([^,]*,?\W*)/g);
+          var names = [];
+          while ((rr = r.exec(parameters)) !== null) {
+            create_nominal(rr[1], undefined, index);
+            names.push(rr[1]);
+            colorize(rr[2]);
+          }
+        }
+        colorize(')');
+      }
+      return true;
+    }
+    function create_ref(nominal, parameters, index) {
+      var scope = cache.scopes.lookup(nominal);
+      if(!scope) return create_nominal(nominal, parameters, index);
+      var name = 'r' + scope.id + '-' + nominal;
+      attr = {
+        className:    STYLE.nominal + ' ' + name,
+        onclick:      function(e) { location.href = '#' + name },
+        onmouseover:  function(e) { change_style(name, true) },
+        onmouseleave: function(e) { change_style(name, false) },
+      };
+      colorize(nominal, undefined, attr);
+      return false;
+    }
+    syntax.processed = true;
+    syntax.keyword   = syntax.keyword   ? { r: regexp(syntax.keyword),  css: STYLE.keyword     } : {};
+    syntax.type      = syntax.type      ? { r: regexp(syntax.type),     css: STYLE.type        } : {};
+    syntax.built_in  = syntax.built_in  ? { r: regexp(syntax.built_in), css: STYLE.built_in    } : {};
+    syntax.type_ctor = syntax.type_ctor ? { r: syntax.type_ctor,        update: create_nominal } : {};
+    syntax.ref_ctor  = syntax.ref_ctor  ? { r: syntax.ref_ctor,         update: create_nominal } : {};
+    syntax.nominal   = { r: '', css: STYLE.nominal, update: create_ref };
+    return syntax;
+  }
+  var scopes = (function(){
+    var _stack = [];
+    var _lang;
+    function gen_id() { return Math.random().toString().substr(2,4) }
+    return {
+      id:      gen_id(),
+      init:    function(lang) { _lang = lang; _stack = []; this.create(); return this },
+      destroy: function() { _stack.pop(); this.update_regex(); },
+      current: function() { return _stack[_stack.length-1] },
+      create:  function(is_block) { 
+        if(is_block && !this.current().is_block)
+          this.current().is_block = true;
+        else
+          _stack.push({ id: this.id + '-' + gen_id(), is_block: !_stack.length, nominals:{}, nominal_regex:'' }); 
+      },
+      lookup:  function(name) {
+        for(var i=_stack.length-1; i>=0; i--)
+          if(_stack[i].nominals[name]) return _stack[i];
+      },
+      add_nominal: function(name) {
+        var scope = this.current();
+        if(scope.nominals[name]) return;
+        scope.nominal_regex = !scope.nominal_regex ? ('\\b' + name + '\\b') : (scope.nominal_regex + '|\\b' + name + '\\b');
+        scope.nominals[name] = true;
+        this.update_regex();
+      },
+      update_regex: function() {
+        var syntax  = LANG[lang];
+        var regexes = [];
+        for(var i=0; i<_stack.length; i++) 
+          if(_stack[i].nominal_regex)
+            regexes.push(_stack[i].nominal_regex);
+        if(!regexes.length) return;
+        var r = new RegExp(regexes.join('|'), 'g');
+        r.index     = syntax.nominal.r.index;
+        r.lastIndex = syntax.nominal.r.lastIndex;
+        syntax.nominal.r = r;
+      }
+    };
+  })();
+  function parse(text) {
+    var last_index = 0, attr;
+    var token1, token2, token3;
+    var lexers = cache.regexps[0];
+    var nominal_index = cache.regexps[1];
+    var pos = lexers.map(function(){ return 0 });
+    text = text
+    while(true) {
       var ii = -1;
       var p0 = p = text.length;
-      for(var i = 0; i < regexps.length; i++) {
-        var m = cache.m;
-        if(m[i] == null) continue;
-        if(!regexps[i].r) continue;
-        if(m[i] == 0 || m[i].index < last_index) {
-          regexps[i].r.lastIndex = last_index;
-          m[i] = regexps[i].r.exec(text);
+      for(var i = 0; i < lexers.length; i++) {
+        if(pos[i] == null) continue;
+        if(!lexers[i].r) continue;
+        if(pos[i] == 0 || pos[i].index < last_index) {
+          lexers[i].r.lastIndex = last_index;
+          pos[i] = lexers[i].r.exec(text);
         }
-        if(m[i] == null) continue;
-        var t1 = undefined, t2 = m[i][0], t3 = undefined, pp = m[i].index;
-        if(m[i].length>=2) {
-          //pp += m[i][1].length;
+        if(pos[i] == null) continue;
+        var t1 = undefined, t2 = pos[i][0], t3 = undefined, pp = pos[i].index;
+        if(pos[i].length>=2) {
+          //pp += pos[i][1].length;
           var j = 1; // Ugly but fast. Refactor needed.
-          while(m[i][j]===undefined && ++j<m[i].length); t1 = m[i][j++];
-          while(m[i][j]===undefined && ++j<m[i].length); t2 = m[i][j++];
-          while(m[i][j]===undefined && ++j<m[i].length); t3 = m[i][j++];
+          while(pos[i][j]===undefined && ++j<pos[i].length); t1 = pos[i][j++];
+          while(pos[i][j]===undefined && ++j<pos[i].length); t2 = pos[i][j++];
+          while(pos[i][j]===undefined && ++j<pos[i].length); t3 = pos[i][j++];
         }
-        if(m[i].index < p) { p = m[i].index; ii = i; token1 = t1; token2 = t2; token3 = t3; }
+        if(pos[i].index < p) { p = pos[i].index; ii = i; token1 = t1; token2 = t2; token3 = t3; }
       }
 
       if(ii == -1){ colorize(text.slice(last_index)); break;} // no highlight needed anymore
@@ -290,15 +252,38 @@
         }else if(token2 == '}') 
           cache.scopes.destroy(); 
       }
-      if(regexps[ii].update)
-        regexps[ii].update(token2, token3, nominal_index);
-      else
-        colorize(token2, regexps[ii].css);
+      if(lexers[ii].update) {
+        if(lexers[ii].update(token2, token3))
+          pos[nominal_index] = 0;  // reset the last position of searched text for nominal rule
+      }else
+        colorize(token2, lexers[ii].css);
 
-      if(last_index == regexps[ii].r.lastIndex) { alert('[Error] Infinite parsing loop in highlight.js'); break }
-      last_index = regexps[ii].r.lastIndex;
+      if(last_index == lexers[ii].r.lastIndex) { alert('[Error] Infinite parsing loop in highlight.js'); break }
+      last_index = lexers[ii].r.lastIndex;
       attr = undefined;
     }// end of while
+  }
+
+  function createHighlightedCode(lang, element, options)
+  {
+    options = options || {};
+    cache = {};
+    cache.scopes = scopes.init();
+    cache.regexps = (function (SYNTAX) {
+      var ret = [], nominal_index;
+      for(var rule in COMMON) ret.push(COMMON[rule]);
+      for(var rule in SYNTAX) SYNTAX[rule] && ret.push(SYNTAX[rule]) && rule=='nominal' && (nominal_index=ret.length-1);
+      return [ret, nominal_index];
+    })(createSyntaxRules(lang));
+
+    cache.codeArea = options['lineno'] ? document.createElement('OL') : document.createElement('UL');
+    var text = element.innerHTML
+               .replace(/&lt;/g,   '<')
+               .replace(/&gt;/g,   '>')
+               //.replace(/&quot;/g, '"')
+               //.replace(/&nbsp;/g, ' ')
+               .replace(/&amp;/g,  '&');
+    parse(text);
 
     var div = document.createElement('DIV');
     div.className = 'sh';
